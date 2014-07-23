22b8fed16881f9d55811a59629a6e9d094b6bf09
intelligentcode <intelligentcodemail.com>
2014-07-23 10:33:49 +0800

    classLoader基本完成

--- src/main/java/ameba/Application.java
+++ src/main/java/ameba/Application.java
@@ -176,27 +176,44 @@ public class Application extends ResourceConfig {
             JvmAgent.initialize();
             logger.info("注册热加载过滤器");
             register(ReloadingFilter.class);
-            registerInstances(new AbstractBinder() {
-                @Override
-                protected void configure() {
-                    try {
-                        bindFactory(new Factory<Object>() {
-                            @Override
-                            public Object provide() {
-                                return Application.this;
-                            }
-
-                            @Override
-                            public void dispose(Object instance) {
-
-                            }
-                        }).to(classLoader.loadClass(Application.class.getName()));
-                    } catch (ClassNotFoundException e) {
-                        logger.error("bind dev app error", e);
-                    }
-                }
-            });
+//            registerInstances(new AbstractBinder() {
+//                @Override
+//                protected void configure() {
+//                    try {
+//                        bindFactory(new Factory<Object>() {
+//                            @Override
+//                            public Object provide() {
+//                                return Application.this;
+//                            }
+//
+//                            @Override
+//                            public void dispose(Object instance) {
+//
+//                            }
+//                        }).to(classLoader.loadClass(Application.class.getName()));
+//                    } catch (ClassNotFoundException e) {
+//                        logger.error("bind dev app error", e);
+//                    }
+//                }
+//            });
         }
+
+        registerInstances(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bindFactory(new Factory<Object>() {
+                    @Override
+                    public Object provide() {
+                        return Application.this;
+                    }
+
+                    @Override
+                    public void dispose(Object instance) {
+
+                    }
+                }).to(Application.class);
+            }
+        });
 
         //设置ssl相关
         secureEnabled = Boolean.parseBoolean(properties.getProperty("ssl.enabled", "false"));
