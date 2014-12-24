ca66f167c6bd7ab5b89bb4eb5b9e62c1ca10fb9e
intelligentcode <intelligentcodemail@gmail.com>
2014-12-24 19:43:26 +0800

    addOn增加EventBus

--- src/main/java/ameba/core/AddOn.java
+++ src/main/java/ameba/core/AddOn.java
@@ -1,9 +1,32 @@
 package ameba.core;
 
+import ameba.container.Container;
+import ameba.event.EventBus;
+import ameba.event.Listener;
+
 /**
  * @author icode
  */
-public interface AddOn {
-    public void setup(Application application);
-    public void done(Application application);
+public abstract class AddOn {
+    private static EventBus EVENT_BUS;
+
+    private static void init() {
+        EVENT_BUS = EventBus.create();
+
+        EVENT_BUS.subscribe(Container.BeginReloadEvent.class,
+                new Listener<Container.BeginReloadEvent>() {
+                    @Override
+                    public void onReceive(Container.BeginReloadEvent event) {
+                        init();
+                    }
+                });
+    }
+
+    static {
+        init();
+    }
+
+    public abstract void setup(Application application);
+
+    public abstract void done(Application application);
 }
