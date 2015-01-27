138266cb498be37228ca3ca6b684579fbb2efaa0
intelligentcode <intelligentcodemail@gmail.com>
2015-01-27 19:38:38 +0800

    更改Akka接口

--- src/main/java/ameba/feature/AmebaFeature.java
+++ src/main/java/ameba/feature/AmebaFeature.java
@@ -1,55 +1,86 @@
 package ameba.feature;
 
 import ameba.container.Container;
+import ameba.core.Application;
 import ameba.event.Event;
 import ameba.event.EventBus;
 import ameba.event.Listener;
 import ameba.event.SystemEventBus;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.glassfish.hk2.api.ServiceLocator;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Feature;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author icode
  */
 public abstract class AmebaFeature implements Feature {
 
-    private static EventBus EVENT_BUS;
-
-    private static void init() {
-        EVENT_BUS = EventBus.createMix();
-
-        SystemEventBus.subscribe(Container.BeginReloadEvent.class,
-                new Listener<Container.BeginReloadEvent>() {
-                    @Override
-                    public void onReceive(Container.BeginReloadEvent event) {
-                        init();
-                    }
-                });
-    }
-
-    static {
-        init();
-    }
+    private static EventBus EVENT_BUS = EventBus.createMix();
+    private static Map<Class<? extends Event>, List<Listener>> listeners;
 
     @Inject
     private ServiceLocator locator;
+    @Inject
+    private Application application;
+
+    public static void publishEvent(Event event) {
+        EVENT_BUS.publish(event);
+    }
+
+    private void initDev() {
+        if (listeners == null) {
+            listeners = Maps.newConcurrentMap();
+            SystemEventBus.subscribe(Container.BeginReloadEvent.class,
+                    new Listener<Container.BeginReloadEvent>() {
+                        @Override
+                        public void onReceive(Container.BeginReloadEvent event) {
+                            if (listeners != null)
+                                for (Class ev : listeners.keySet()) {
+                                    for (Listener listener : listeners.get(ev)) {
+                                        EVENT_BUS.unsubscribe(ev, listener);
+                                    }
+                                }
+                        }
+                    });
+        }
+    }
+
+    private <E extends Event> void subscribe(Class<E> eventClass, final Listener<E> listener) {
+        if (application.getMode().isDev()) {
+            initDev();
+            List<Listener> list = listeners.get(eventClass);
+            if (list == null) {
+                list = Lists.newArrayList();
+                listeners.put(eventClass, list);
+            }
+            list.add(listener);
+        }
+        EVENT_BUS.subscribe(eventClass, listener);
+    }
 
     protected <E extends Event> void subscribeEvent(Class<E> eventClass, final Listener<E> listener) {
         locator.inject(listener);
         locator.postConstruct(listener);
-        EVENT_BUS.subscribe(eventClass, listener);
+        subscribe(eventClass, listener);
     }
 
     protected <E extends Event> Listener subscribeEvent(Class<E> eventClass, final Class<? extends Listener<E>> listenerClass) {
         Listener<E> listener = locator.createAndInitialize(listenerClass);
-        EVENT_BUS.subscribe(eventClass, listener);
+        subscribe(eventClass, listener);
         return listener;
     }
 
     protected <E extends Event> void unsubscribeEvent(Class<E> eventClass, final Listener<E> listener) {
         locator.preDestroy(listener);
+        if (application.getMode().isDev()) {
+            List<Listener> list = listeners.get(eventClass);
+            if (list != null) list.remove(listener);
+        }
         EVENT_BUS.unsubscribe(eventClass, listener);
     }
 
@@ -68,9 +99,5 @@ public abstract class AmebaFeature implements Feature {
         Listener<E> listener = locator.createAndInitialize(listenerClass);
         SystemEventBus.subscribe(eventClass, listener);
         return listener;
-    }
-
-    public static void publishEvent(Event event) {
-        EVENT_BUS.publish(event);
     }
 }
