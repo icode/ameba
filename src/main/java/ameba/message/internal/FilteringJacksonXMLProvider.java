a126c38074e99adc3cd7a9f80aa043fef8aae0d4
icode <intelligentcodemail@gmail.com>
2015-04-15 00:37:32 +0800

    生成doc

---
+++ src/main/java/ameba/message/internal/FilteringJacksonXMLProvider.java
@@ -0,0 +1,69 @@
+package ameba.message.internal;
+
+import ameba.core.Application;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.jaxrs.xml.XMLEndpointConfig;
+import org.glassfish.jersey.message.filtering.spi.ObjectProvider;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+/**
+ * <p>FilteringJacksonXMLProvider class.</p>
+ *
+ * @author icode
+ * @since 0.1.6e
+ */
+@Singleton
+public class FilteringJacksonXMLProvider extends JacksonXMLProvider {
+
+    @Inject
+    private Provider<ObjectProvider<FilterProvider>> provider;
+
+    /**
+     * <p>Constructor for FilteringJacksonXMLProvider.</p>
+     *
+     * @param app       a {@link ameba.core.Application} object.
+     * @param xmlMapper a {@link com.fasterxml.jackson.dataformat.xml.XmlMapper} object.
+     */
+    @Inject
+    public FilteringJacksonXMLProvider(Application app, XmlMapper xmlMapper) {
+        super(app, xmlMapper);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected XMLEndpointConfig _configForWriting(final XmlMapper mapper, final Annotation[] annotations,
+                                                  final Class<?> defaultView) {
+
+        return super._configForWriting(
+                (XmlMapper) JacksonUtils.configFilterIntrospector(mapper)
+                , annotations, defaultView);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeTo(final Object value,
+                        final Class<?> type,
+                        final Type genericType,
+                        final Annotation[] annotations,
+                        final javax.ws.rs.core.MediaType mediaType,
+                        final MultivaluedMap<String, Object> httpHeaders,
+                        final OutputStream entityStream) throws IOException {
+
+        JacksonUtils.setObjectWriterInjector(provider, genericType, annotations);
+        super.writeTo(value, type, genericType, annotations, mediaType, httpHeaders, entityStream);
+    }
+
+}
