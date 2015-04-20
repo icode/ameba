b8a985e8031594b96b76ec81875aecd1aa1cb9d5
icode <intelligentcodemail@gmail.com>
2015-04-20 18:03:17 +0800

    更改接口

--- src/main/java/ameba/db/ORMFeature.java
+++ src/main/java/ameba/db/ORMFeature.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
 /**
- * <p>Abstract TransactionFeature class.</p>
+ * <p>Abstract ORMFeature class.</p>
  *
  * @author icode
  */
