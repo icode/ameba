06e2c67fdf04852240de557fc5a7570251dce3bc
icode <intelligentcodemail@gmail.com>
2015-07-08 10:52:49 +0800

    更改模板寻找方式

--- src/main/java/ameba/mvc/template/internal/NotFoundForward.java
+++ src/main/java/ameba/mvc/template/internal/NotFoundForward.java
@@ -5,15 +5,20 @@ import jersey.repackaged.com.google.common.collect.Sets;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.internal.inject.Providers;
 import org.glassfish.jersey.server.mvc.spi.TemplateProcessor;
-import org.glassfish.jersey.spi.ExtendedExceptionMapper;
 
+import javax.annotation.Priority;
 import javax.inject.Inject;
 import javax.inject.Provider;
-import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.Set;
 
 /**
@@ -23,13 +28,13 @@ import java.util.Set;
  * @since 2013-08-27
  */
 @Singleton
-public class NotFoundForward implements ExtendedExceptionMapper<NotFoundException> {
+@Priority(Priorities.ENTITY_CODER)
+public class NotFoundForward implements ContainerResponseFilter {
 
     @Context
     private Provider<UriInfo> uriInfo;
     @Inject
     private ServiceLocator serviceLocator;
-    private ThreadLocal<String> templatePath = new ThreadLocal<>();
 
     private Set<TemplateProcessor> getTemplateProcessors() {
         Set<TemplateProcessor> templateProcessors = Sets.newLinkedHashSet();
@@ -39,43 +44,39 @@ public class NotFoundForward implements ExtendedExceptionMapper<NotFoundExceptio
         return templateProcessors;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Response toResponse(NotFoundException exception) {
-        try {
-            return Response.ok(Viewables.newDefaultViewable(templatePath.get())).type(MediaType.TEXT_HTML_TYPE).build();
-        } finally {
-            templatePath.remove();
-        }
-    }
-
     private String getCurrentPath() {
         return "/" + uriInfo.get().getPath();
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isMappable(NotFoundException exception) {
+    public String mappedViewPath() {
         String path = getCurrentPath();
         //受保护目录,不允许直接访问
         String pDir = Viewables.PROTECTED_DIR + "/";
         if (path.startsWith(pDir)
-                || path.startsWith("/" + pDir)) return false;
+                || path.startsWith("/" + pDir)) return null;
         for (TemplateProcessor templateProcessor : getTemplateProcessors()) {
-            Object has = templateProcessor.resolve(path, null);
+            Object has = templateProcessor.resolve(path, MediaType.TEXT_HTML_TYPE);
             if (has == null) {
                 path = path + (path.endsWith("/") ? "" : "/") + "index";
-                has = templateProcessor.resolve(path, null);
+                has = templateProcessor.resolve(path, MediaType.TEXT_HTML_TYPE);
             }
             if (has != null) {
-                templatePath.set(path);
-                return true;
+                return path;
             }
         }
-        return false;
+        return null;
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        if (responseContext.getStatus() == 404) {
+            String path = mappedViewPath();
+            if (path != null) {
+                responseContext.setEntity(Viewables.newDefaultViewable(path),
+                        new Annotation[0], MediaType.TEXT_HTML_TYPE);
+                responseContext.setStatus(Response.Status.OK.getStatusCode());
+            }
+        }
     }
 }
