b4fa984ca373679061cc3d2b267de434bd6f2a41
icode <intelligentcodemail@gmail.com>
2015-12-24 20:04:29 +0800

    更改MediaType重写判断

--- src/main/java/ameba/message/filtering/MediaTypeFilter.java
+++ src/main/java/ameba/message/filtering/MediaTypeFilter.java
@@ -5,10 +5,12 @@ import org.glassfish.jersey.message.internal.AcceptableMediaType;
 import org.glassfish.jersey.message.internal.HttpHeaderReader;
 
 import javax.annotation.Priority;
+import javax.inject.Inject;
 import javax.ws.rs.Priorities;
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
@@ -20,14 +22,17 @@ import java.util.List;
  *
  * @author icode
  */
-@PreMatching
 @Priority(Priorities.HEADER_DECORATOR)
 public class MediaTypeFilter implements ContainerRequestFilter {
     private static final MediaType LOW_IE_DEFAULT_REQ_TYPE = new MediaType("application", "x-ms-application");
 
+    @Inject
+    private ResourceInfo resourceInfo;
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String acceptHeader = requestContext.getHeaderString(HttpHeaders.ACCEPT);
+
         if (StringUtils.isBlank(acceptHeader)) {
             applyHeader(requestContext);
         } else {
@@ -35,9 +40,10 @@ public class MediaTypeFilter implements ContainerRequestFilter {
                 List<AcceptableMediaType> acceptableMediaTypes = HttpHeaderReader.readAcceptMediaType(acceptHeader);
                 if (acceptableMediaTypes.size() > 0) {
                     AcceptableMediaType type = acceptableMediaTypes.get(0);
-                    if ((type.isWildcardType() && type.isWildcardSubtype())
-                            || (LOW_IE_DEFAULT_REQ_TYPE.getType().equals(type.getType())
-                            && LOW_IE_DEFAULT_REQ_TYPE.getSubtype().equals(type.getSubtype()))) {
+                    if (LOW_IE_DEFAULT_REQ_TYPE.getType().equals(type.getType())
+                            && LOW_IE_DEFAULT_REQ_TYPE.getSubtype().equals(type.getSubtype())) {
+                        applyHeader(requestContext, false);
+                    } else if (type.isWildcardType() && type.isWildcardSubtype()) {
                         applyHeader(requestContext);
                     }
                 } else {
@@ -50,6 +56,30 @@ public class MediaTypeFilter implements ContainerRequestFilter {
     }
 
     private void applyHeader(ContainerRequestContext requestContext) {
+        applyHeader(requestContext, true);
+    }
+
+    private void applyHeader(ContainerRequestContext requestContext, boolean checkResource) {
+        if (checkResource) {
+            Produces methodProduces = resourceInfo.getResourceMethod().getAnnotation(Produces.class);
+            if (hasProduces(methodProduces)) {
+                return;
+            }
+            Produces classProduces = resourceInfo.getResourceClass().getAnnotation(Produces.class);
+            if (hasProduces(classProduces)) {
+                return;
+            }
+        }
         requestContext.getHeaders().putSingle(HttpHeaders.ACCEPT, MediaType.TEXT_HTML);
+    }
+
+    private boolean hasProduces(Produces produces) {
+        if (produces != null && produces.value().length > 0) {
+            String firstType = produces.value()[0];
+            if (StringUtils.isNotBlank(firstType) && !"*/*".equals(firstType)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
