dbdb9dc4d538bfbe69274adfb2562ab0662a2862
icode <intelligentcodemail@gmail.com>
2017-10-31 18:35:02 +0800

    fea: update async event bus

--- src/main/java/ameba/event/AsyncListener.java
+++ src/main/java/ameba/event/AsyncListener.java
@@ -5,7 +5,6 @@ import co.paralleluniverse.fibers.Suspendable;
 /**
  * @author icode
  */
-@Suspendable
 public interface AsyncListener<E extends Event> extends Listener<E> {
     @Suspendable
     void onReceive(E event);
